2cad9f78f8b1baa1cfdeddfb513261ffc94d5aa5
elchangtw <elchang.tw@gmail.com>
2020-01-15 15:21:25 -0800

    fetch request route complete

--- challenge_1/client/app.jsx
+++ challenge_1/client/app.jsx
@@ -1,6 +1,25 @@
 import React from 'react';
 
+const server_url = "http://localhost:3000";
+
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    fetch(server_url + '/events', {
+      method: 'GET',
+    })
+    .then((response) => {
+      return response.json();
+    })
+    .then((result) => {
+      console.log(result);
+    });
+  }
+
+
   render() {
     return (
       <div>
